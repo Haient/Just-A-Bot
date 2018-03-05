e03f8e88988c9691d2edfecd81d6b94e00a7524c
Haient <32432174+Haient@users.noreply.github.com>
2018-03-05 15:51:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,11 @@ function play(connection, message) {
 		});
 }
 
+///////// Статус Бота /////////////////////////////////////////////////////////////////////////////////////
 
-client.on("ready", function() {
-	console.log("Ready");
-	client.user.setGame('-help for info');
+  client.on("ready", function() {
+	  console.log("Ready");
+	  client.user.setPresence({ game: { name: '-help', type: 3 } })
 });
 
 var servers = {};
@@ -38,6 +39,9 @@ client.on("message", function(message) {
 	var args = message.content.substring(PREFIX.length).split(" ");
 
 	switch (args[0]) {
+
+///////// Фановые команды /////////////////////////////////////////////////////////////////////////////////
+
 		case "димон":
 		  message.channel.sendMessage("***ДИМООО...***");
 		  message.channel.sendMessage("***...ООО...***");
@@ -65,51 +69,170 @@ client.on("message", function(message) {
 		  message.channel.sendMessage("***-МЫ НА ВОШЛЕБНИКА БЛЯТЬ НАПАЛИ!***")
 		  message.channel.sendMessage("***ОН... ПОСМОТРИ БЛЯЯЯТЬ!***")
 		break;
+
+        case "легушка":
+        var embed = new Discord.RichEmbed()
+          .setColor("#007600")
+          .setImage("https://i.imgur.com/r6xmLr1.gif")
+          .setThumbnail("https://i.imgur.com/r6xmLr1.gif")
+          .setFooter("Туц туц туц" ,"https://i.imgur.com/r6xmLr1.gif")
+          .setAuthor("Туц туц туц" ,"https://i.imgur.com/r6xmLr1.gif")
+        message.channel.sendEmbed(embed);
+        break;
 			
-		case "polish":
+///////// Уроки польского языка ///////////////////////////////////////////////////////////////////////////
+
+        case "алфавит":
+         message.delete();
+        var embed = new Discord.RichEmbed()
+	    .setAuthor("Polish", "https://i.imgur.com/xlGU7St.png")
+          .setColor("#ffffff")
+          .setFooter("Следующая страница <<-polish1>>")
+          .setImage("https://i.imgur.com/0pz3Wd4.jpg")
+        message.channel.sendEmbed(embed);
+        break;
+
+		case "polish1":
 	  var embed = new Discord.RichEmbed()
 	    .setAuthor("Polish", "https://i.imgur.com/xlGU7St.png")
-		  .setColor("#ffffff")
+		 .setColor("#ffffff")
 		  .setTitle("**ПОЛЬСКИЕ БУКВОСОЧЕТАНИЯ**")
-		  .setFooter("Будет дополняться")
-		  .addField("Буквосочетания: *Ch*", "Транскрипция: **Х**")
-		  .addField("Буквосочетания: *Rz*", "Транскрипция: **Ж**")
-	          .addField("Буквосочетания: *Dz*", "Транскрипция: **Дз**")
-		  .addField("Буквосочетания: *Dź*", "Транскрипция: **Джь**")
-		  .addField("Буквосочетания: *Dż*", "Транскрипция: **Дж**")
-		  .addField("Буквосочетания: *Sz*", "Транскрипция: **Ш**")
-		  .addField("Буквосочетания: *Cz*", "Транскрипция: **Ч**")
-	          .addField("Буквосочетания: *SzCz*", "Транскрипция: **Щ**")
+		   .setFooter("Следующая страница <<-polish2>>")
+		  .addField("Буквосочетания: Ch", "Транскрипция: **Х**")
+		  .addField("Буквосочетания: Rz", "Транскрипция: **Ж**")
+	      .addField("Буквосочетания: Dz", "Транскрипция: **Дз**")
+		  .addField("Буквосочетания: Dź", "Транскрипция: **Джь**")
+		  .addField("Буквосочетания: Dż", "Транскрипция: **Дж**")
+		  .addField("Буквосочетания: Sz", "Транскрипция: **Ш**")
+		  .addField("Буквосочетания: Cz", "Транскрипция: **Ч**")
+	      .addField("Буквосочетания: SzCz", "Транскрипция: **Щ**")
 		  message.channel.sendEmbed(embed);	
                 break;
+
+        case "polish2":
+	  var embed = new Discord.RichEmbed()
+	    .setAuthor("Polish", "https://i.imgur.com/xlGU7St.png")
+		 .setColor("#ffffff")
+		  .setTitle("**ЧАСТО ИСПОЛЬЗУЕМЫЕ СЛОВА**")
+		   .setFooter("Следующая страница <<-polish3>>")
+		  .addField("Слово: Przepraszam", "Перевод: **Прошу прощения**")
+		  .addField("Слово: Dzień dobry", "Перевод: **Добрый день**")
+	      .addField("Слово: Dziękuję", "Перевод: **Спасибо**")
+		  .addField("Слово: Dobranoc", "Перевод: **Спокойной ночи**")
+		  .addField("Слово: Prosze", "Перевод: **Пожалуйста**")
+		  .addField("Слово: Część", "Перевод: **Привет/Пока**")
+		  .addField("Слово: Dobrze", "Перевод: **Хорошо**")
+	      .addField("Слово: Pewnie", "Перевод: **Конечно**")
+	      .addField("Слово: Tak", "Перевод: **Да**")
+	      .addField("Слово: Nie", "Перевод: **Нет**")
+		  message.channel.sendEmbed(embed);	
+                break;
+
+        case "polish3":
+	  var embed = new Discord.RichEmbed()
+	    .setAuthor("Polish", "https://i.imgur.com/xlGU7St.png")
+		 .setColor("#ffffff")
+		  .setTitle("**ЧАСТО ИСПОЛЬЗУЕМЫЕ СЛОВА**")
+		   .setFooter("Следующая страница <<-polish4>>")
+		  .addField("Слово: Сo tam u ciebie?", "Перевод: **Как у тебя дела?**")
+		  .addField("Слово: Оdpoczywać", "Перевод: **Отдхыть**")
+	      .addField("Слово: Miłość", "Перевод: **Любовь**")
+		  .addField("Слово: Sklep", "Перевод: **Магазин**")  
+		  .addField("Слово: Błąd", "Перевод: **Ошибка**")
+		  .addField("Слово: Jutro", "Перевод: **Завтра**")
+		  .addField("Слово: Tydzień", "Перевод: **Неделя**")
+	      .addField("Слово: Dzisiaj", "Перевод: **Сегодня**")
+	      .addField("Слово: Wczoraj", "Перевод: **Вчера**")
+	      .addField("Слово: Bardzo", "Перевод: **Очень**")
+		  message.channel.sendEmbed(embed);	
+                break;
+
+        case "polish4":
+	  var embed = new Discord.RichEmbed()
+	    .setAuthor("Polish", "https://i.imgur.com/xlGU7St.png")
+		 .setColor("#ffffff")
+		  .setTitle("**ЧАСТО ИСПОЛЬЗУЕМЫЕ СЛОВА**")
+		   .setFooter("Следующая страница <<-polish5>> **(ожидаеться в версии 0.9)**")
+		  .addField("Слово: Jeden", "Перевод: **Один**")
+		  .addField("Слово: Dwa", "Перевод: **Два**")
+	      .addField("Слово: Trzy", "Перевод: **Три**")
+		  .addField("Слово: Cztery", "Перевод: **Четыре**")  
+		  .addField("Слово: Pięć", "Перевод: **Пять**")
+		  .addField("Слово: Sześć", "Перевод: **Шесть**")
+		  .addField("Слово: Siedem", "Перевод: **Семь**")
+	      .addField("Слово: Osiem", "Перевод: **Восемь**")
+	      .addField("Слово: Dziewięć", "Перевод: **Девять**")
+	      .addField("Слово: Dziesięć", "Перевод: **Десять**")
+		  message.channel.sendEmbed(embed);	
+                break;                
      
-    // Текст который появляеться при неправельном наборе команд
+///////// Текст который появляеться при неправельном наборе команд ////////////////////////////////////////
+
 		default:
+		 message.delete();
           var embed = new Discord.RichEmbed()
 		  .setColor("#D63232")
 		  .setTitle("***ТАКОЙ КОМАНДЫ НЕ СУЩЕСТВУЕТ***")
-		  .setFooter("Данный бот находиться в стадии глубокой разработки")
+		  .setFooter("Данный бот находиться в стадии глубокой разработки | Версия бота: 0.8.34")
 		  .addField("*-help*", "Информация о командах.")
 		  message.channel.sendEmbed(embed);
         break;
 
-    // Информация о командах Бота
-		case "help":
+///////// Информация о фановых командах Бота //////////////////////////////////////////////////////////////////////
+
+		case "fun":
+		 message.delete();
 		  var embed = new Discord.RichEmbed()
+		  .setAuthor("Фановые команды бота Just a Test Bot", "https://i.imgur.com/1MR95h8.png")
 		  .setColor("#7289DA")
-		  .setTitle("**ОСНОВНЫЕ КОМАНДЫ**")
-		  .setFooter("Данный бот находиться в стадии глубокой разработки")
-		  .addField("*-help*", "Информация о командах.")
-		  .addField("*-polish*", "Сноска польского языка")
-		  .addField("*-димон*", "Добавляет **ДИМОООН**, в Ваше сообщение.")
-		  .addField("-шрек(1-2)", "Добавляет **Куплет из песни**, в Ваше сообщение.")
-		  .addField("-play (url)", "Проигрывание музыки по ссылке (YouTube).")
-		  .addField("-skip", "Пропуск трека.")
-		  .addField("-stop", "Остановка трека.")
+		  .setTitle("**ФАНОВЫЕ КОМАНДЫ**")
+		  .setFooter("Данный бот находиться в стадии глубокой разработки | Версия бота: 0.8.34")
+		  .addField("-димон", "Добавляет ДИМОООооона в чат.")
+          .addField("-шрек(1/2)", "Це моє болотоб, друже.")
+		  .addField("-гарри", "БЛЯТЬ, ДА ОН ЕБАНЫЙ ВОЛШЕБНИК!")
+		  .addField("-легушка", "Ква, ква.")
 		  message.channel.sendEmbed(embed);
 		break;
 
-    // Проигрыватель видео/музыки
+///////// Информация о командах Бота //////////////////////////////////////////////////////////////
+
+		case "help":
+		 message.delete();
+		  var embed = new Discord.RichEmbed()
+		  .setAuthor("Команды бота Just a Test Bot", "https://i.imgur.com/1MR95h8.png")
+		  .setColor("#7289DA")
+		  .setTitle("**ОСНОВНЫЕ КОМАНДЫ**")
+		  .setFooter("Данный бот находиться в стадии глубокой разработки | Версия бота: 0.8.34")
+		  .addField("-help", "Информация о командах.")
+          .addField("-fun", "Список команд, созданных для фана.")
+		  .addField("-new", "Список обновлений бота.")
+		  .addField("-play (url)", "Проигрывание музыки по ссылке (YouTube).")
+		  .addField("-skip", "Пропуск трека.")
+		  .addField("-stop", "Остановка трека.")
+		  .addField("-polish(1-4)", "Сноска для изучения польского языка.")
+		  message.channel.sendEmbed(embed);
+		break;
+
+///////// Обновления бота /////////////////////////////////////////////////////////////////////////////////
+
+		case "new":
+		  var embed = new Discord.RichEmbed()
+		  .setAuthor("Настало время обновлений", "https://i.imgur.com/uJprplj.png")
+		  .setColor("#F59B00")
+		  .setTitle("**СПИСОК ИЗМЕНЕНИЙ В ВЕРСИИ 0.8.3**")
+		  .setFooter("Последнее обновление: 05.03.2018 10:00 (UTC+3) | Версия бота: 0.8.34")
+		  .addField("Мы добавили больше команд", "Теперь в боте, есть еще больше разных плюшек, которыми можно баловаться. *Не забывайте про -help.*")
+		  .addField("Добавлена сноска польского языка", "Это сделано для тех, кто хочет, выучить язык и поиграть в оригинальной озвучке в The Witcher 3.")
+		  .addField("Оптимизирована работа бота", "Меньше лагов - больше веселья и возможностей.")
+		  .addField("Теперь мы ведем DEV-LOG", "Наконец-то вы сможете следить за новыми обновлениями.")
+		  .addField("Теперь бот по настоящему Смотрит", "Мы сделали боту глаз что бы он мог смотреть на список команд *(-help)*.")
+		  .addField("Был добавлен Вебхук для YouTube", "Это никак не связано с ботом, но теперь можно сделить за своими любимыми ютуберами не выходя из Discord.")
+		  .addField("Мелкие фиксы и дополнения", "Они настолько маленькие, что их сложно заметить.")
+		  message.channel.sendEmbed(embed);
+		break;
+
+///////// Проигрыватель видео/музыки //////////////////////////////////////////////////////////////////////
+
 		case "play":
 		  if (!args[1]) {
 		  	message.channel.sendMessage("**Пожалуйста, учажите ссылку на ролик YouTube.**");
@@ -153,7 +276,7 @@ client.on("message", function(message) {
         break;
 
         case "stop":
-          var server = server[message.guild.id];
+          var server = servers[message.guild.id];
 
           if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
         break;
@@ -181,5 +304,6 @@ function playMusic(id, message){
         });
     });
 }
+
 
 client.login(process.env.BOT_TOKEN);
